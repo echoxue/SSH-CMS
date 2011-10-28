9956a2c5aac8266d18093812b64b7040091beae3
Echo <Echo@Echoxue>
2011-10-28 20:31:33 +0800

    this is first commit

---
+++ ant-project/src/com/aspire/AntClass.java
@@ -0,0 +1,13 @@
+package com.aspire;
+
+public class AntClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("ant test....");
+	}
+
+}
